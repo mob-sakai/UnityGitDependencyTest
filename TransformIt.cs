668d4a5f82165660cbd73f49a7d68276042cf44c
mob-sakai <sakai861104@gmail.com>
2020-08-03 13:13:49 +0900

    package v1

---
+++ TransformIt.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Coffee.Ugd
+{
+    public class TransformIt : MonoBehaviour
+    {
+        [SerializeField] private Vector3 m_Translate;
+
+        [SerializeField] private Vector3 m_Rotate;
+
+        private void Update()
+        {
+            Translate(transform, m_Translate);
+            Rotate(transform, m_Rotate);
+        }
+
+        public static void Translate(Transform t, Vector3 v)
+        {
+            t.Translate(v.x, v.y, v.z);
+        }
+
+        public static void Rotate(Transform t, Vector3 v)
+        {
+            t.Rotate(v.x, v.y, v.z);
+        }
+    }
+}
