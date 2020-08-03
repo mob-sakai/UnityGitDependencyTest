78cfd89010c8a6f75b38517fd4d8b97150e0683d
mob-sakai <sakai861104@gmail.com>
2020-08-03 13:13:49 +0900

    package v1

---
+++ TranslateIt.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Coffee.Ugd
+{
+    public class TranslateIt : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 m_Translate;
+
+        private void Update()
+        {
+            Translate(transform, m_Translate);
+        }
+
+        public static void Translate(Transform t, Vector3 v)
+        {
+            t.Translate(v.x, v.y, v.z);
+        }
+    }
+}
