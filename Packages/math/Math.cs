4b47c547531b65d56f3f7e629830e574efa2b8c6
mob-sakai <sakai861104@gmail.com>
2020-08-03 13:13:49 +0900

    package v1

---
+++ Packages/math/Math.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Coffee.Ugd
+{
+    public static class Math
+    {
+        public static float GetX(Vector3 v)
+        {
+            return v.x;
+        }
+
+        public static float GetY(Vector3 v)
+        {
+            return v.y;
+        }
+
+        public static float GetZ(Vector3 v)
+        {
+            return v.z;
+        }
+    }
+}
---
+++ Packages/rotate-it/RotateIt.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Coffee.Ugd
+{
+    public class RotateIt : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 m_Rotate;
+
+        private void Update()
+        {
+            Rotate(transform, m_Rotate);
+        }
+
+        public static void Rotate(Transform t, Vector3 v)
+        {
+            t.Rotate(v.x, v.y, v.z);
+        }
+    }
+}
