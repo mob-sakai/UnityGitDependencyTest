230a698b5d52f92a3497083a4505f078bfd19f70
mob-sakai <sakai861104@gmail.com>
2020-08-03 13:59:31 +0900

    deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
-git subtree split --prefix="Packages/com.coffee.packageA" --branch package-a
-git subtree split --prefix="Packages/com.coffee.packageB" --branch package-b
-git subtree split --prefix="Packages/com.coffee.packageC" --branch package-c
-git subtree split --prefix="Packages/com.coffee.packageX" --branch package-x
+git subtree split --prefix="Packages/math" --branch math
+git subtree split --prefix="Packages/transform-it" --branch transform-it
+git subtree split --prefix="Packages/translate-it" --branch translate-it
+git subtree split --prefix="Packages/rotate-it" --branch rotate-it
+
+git push origin math
+git push origin transform-it
+git push origin translate-it
+git push origin rotate-it
